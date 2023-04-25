be535f3784e856ca1b81ec383dcf0e561b5d66e7
Erin Grant <eringrant@users.noreply.github.com>
2023-04-25 13:37:50 +0100

    adapt template

--- nets/__init__.py
+++ nets/__init__.py
@@ -1,15 +1,15 @@
-import TODO_package_name
+import nets
 import os
 
 __version__ = "0.0.1"
 
 # Module-wide path constants as in
 # https://stackoverflow.com/a/59597733.
-__package_path = os.path.split(TODO_package_name.__path__[0])[0]
+__package_path = os.path.split(nets.__path__[0])[0]
 DATA_DIR = os.path.join(__package_path, "data")
-TMP_DIR = os.path.join("/tmp", "TODO_package_name")
+TMP_DIR = os.path.join("/tmp", "nets")
 os.makedirs(TMP_DIR, exist_ok=True)
 
-del TODO_package_name
+del nets
 del os
 del __package_path
